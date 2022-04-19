6f23ac7091dbd97126ded24aff49002ef48a7123
yinzhijie <yzjgogo@139.com>
2022-04-19 09:55:57 +0800

    babel-loader

--- WebpackStudy/07_webpack搭建本地服务页面热更新/webpack.config.js
+++ WebpackStudy/07_webpack搭建本地服务页面热更新/webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
      webpack搭建的本地服务器，可以实现我们想要的让浏览器自动刷新显示我们修改后的结果。
      在package.json -> scripts -> "dev": "webpack-dev-server --open"
      以后在命令行进行npm run dev即可实现热更新，其中--open指自动打开浏览器,编译完成后terminal有浏览器地址：?http://localhost:8080
+
+     npm run dev(webpack-dev-server) 与 npm run build(webpack) 的关系：
+     前者在开发阶段用于方便实时热更新调试使用，不会将代码打包到dist/bundle.js中，而是在内存中实时更新，速度更快，不信可以改点代码人后热更新后看看dist/bundle.js有没有变化
+     后者在开发完成后，最终打包时使用，会将代码打包到dist/中
+
      参考图片：本地服务器.png
      * */
     devServer: {
