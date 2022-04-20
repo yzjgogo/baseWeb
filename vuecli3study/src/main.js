100767197b68a9eebc0f4ed15611d760a6134e59
yinzhijie <yzjgogo@139.com>
2022-04-20 14:57:20 +0800

    babel-loader

--- vuecli3study/src/main.js
+++ vuecli3study/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/**如果为true则构建或编译时terminal会输出很多信息*/
 Vue.config.productionTip = false
+
+
 /**可见CLI3创建的项目默认就是runtimeonly的方式*/
 new Vue({
   render: h => h(App),
