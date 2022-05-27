4337a9e9b72659676eb16b77d56ffd95b981118a
yinzhijie <yzjgogo@139.com>
2022-05-27 10:29:54 +0800

    dfgdfgfg

--- 4_路由/01-learnvuerouter/src/main.js
+++ 4_路由/01-learnvuerouter/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-//所有的组件都会有一个test()方法，因为所有的组件都集成自vue的原型
+//所有的组件都会有一个test()方法，因为所有的组件都继承自vue的原型
 // Vue.prototype.test = function () {
 //   console.log('test');
 // }
