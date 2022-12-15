b6083c649670e38ac7035544c2a230e6d1ac5350
yinzhijie <yzjgogo@139.com>
2022-12-15 14:19:20 +0800

    引入vue router

--- vuecli3study/src/components/Home.vue
+++ vuecli3study/src/components/Home.vue
@@ -28,7 +28,8 @@ export default {
 }
 </script>
 
-<style>
+<!--<style>-->
+  <style lang="less" scoped>
 #home-d {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -36,5 +37,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  display: block;
+  margin: 20px;
 }
 </style>
