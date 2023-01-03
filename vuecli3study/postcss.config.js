a4478fd3493be700841ce3e9114c45d8a502282f
yinzhijie <yzjgogo@139.com>
2023-01-03 14:49:09 +0800

    补充postcss的解释

--- vuecli3study/postcss.config.js
+++ vuecli3study/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'autoprefixer': {
-      // overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7']
+      overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7']
     },
     /*
       px自动转rem步骤：参考：px转rem.png
@@ -20,6 +20,18 @@ module.exports = {
       https://www.npmjs.com/package/amfe-flexible
       https://github.com/amfe/lib-flexible/tree/master
       为html、body添加font-size，窗口调整时候重新设置font-size，这是rem自适应屏幕的核心
+
+      postcss：
+      https://www.postcss.com.cn/
+      postcss可以理解为是一个处理css的工具库，他包含了很多处理css的插件，例如：
+      autoprefixer：自动添加浏览器前缀，参考：E:\web\JsBase\01_HtmlCssBase\97浏览器前缀.html
+      postcss-pxtorem：将px转化为rem
+      postcss-import:用于原生css的导入，不需要css预处理器就能实现(less)
+      postcss-mixins:用于原生css的混入(css代码的抽取)，不需要css预处理器就能实现(less)
+
+      使用postcss-pxtorem时，虽然没有在vuecli3study\package.json里引入"postcss": "8.3.6", "postcss-loader": "6.1.1",
+      但是实际上vue的cli3已经自动引入了，参考：vuecli3study/node_modules/@vue/cli-service/package.json里的"postcss": "^8.2.6","postcss-loader": "^6.1.1",
+      一般使用postcss下的某个插件，都要引入"postcss": "x.x.x"
 
 
     * */
