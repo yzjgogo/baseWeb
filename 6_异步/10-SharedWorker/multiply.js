12206f584b23354a6d4779e8f9384ed6b66b3a45
yinzhijie <yzjgogo@139.com>
2023-03-29 15:35:19 +0800

    引入vue router

--- 6_异步/10-SharedWorker/multiply.js
+++ 6_异步/10-SharedWorker/multiply.js
@@ -7,6 +7,7 @@ if (!!window.SharedWorker) {
   const myWorker = new SharedWorker("worker.js");
 
   first.onchange = function () {
+    //向共享子线程发送消息
     myWorker.port.postMessage([first.value, second.value]);
     console.log("Message posted to worker");
   };
@@ -16,9 +17,24 @@ if (!!window.SharedWorker) {
     console.log("Message posted to worker");
   };
 
+  /**
+   接收子线程的消息
+
+   使用myWorker.port.onmessage会自动打开端口连接，无需再调用myWorker.port.start()方法
+   使用myWorker.port.addEventListener()不会自动打开端口连接，需要手动调用myWorker.port.start()方法
+   * */
   myWorker.port.onmessage = function (e) {
     result1.textContent = e.data;
     console.log("Message received from worker");
     console.log(e.lastEventId);
   };
+
+  /*
+  myWorker.port.addEventListener('message',function (e) {
+    result1.textContent = e.data;
+    console.log("Message received from worker");
+    console.log(e.lastEventId);
+  })
+  myWorker.port.start()
+   */
 }
--- 6_异步/10-SharedWorker/worker.js
+++ 6_异步/10-SharedWorker/worker.js
@@ -1,16 +1,32 @@
+/**
+ 当一个端口连接被创建时（例如：在multiply.js主线程中，设置 onmessage 事件处理函数，或者显式调用 start() 方法时），
+ 使用 onconnect 事件处理函数来执行代码。
+ */
 onconnect = function (event) {
 //   在worker.js里写上console.log()然后浏览器F12控制台无法看到输出的解决办法：
 // 参考：看到worker里的log.png
-  console.log("执行onconnect1")
-  console.log(event)
-  console.log(event.ports)
   const port = event.ports[0];
   console.log(port)
-  console.log("执行onconnect2")
 
+  /**
+   接收主线程的消息
+
+   使用port.onmessage会自动打开端口连接，无需再调用port.start()方法
+   使用port.addEventListener()不会自动打开端口连接，需要手动调用port.start()方法
+   * */
   port.onmessage = function (e) {
     console.log('worker收到消息',e)
     const workerResult = `Result: ${e.data[0] * e.data[1]}`;
+    //向主线程发送消息
     port.postMessage(workerResult);
   };
+
+  /*
+  port.addEventListener('message',function(e){
+      console.log('worker收到消息',e)
+      const workerResult = `Result: ${e.data[0] * e.data[1]}`;
+      port.postMessage(workerResult);
+  })
+  port.start()
+   */
 };
