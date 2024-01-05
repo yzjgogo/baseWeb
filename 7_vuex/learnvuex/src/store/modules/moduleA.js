8e05f852387dded0471c6de715001f9c3e0a96a7
yinzhijie <yzjgogo@139.com>
2024-01-05 13:59:53 +0800

    添加必要的注释

--- 7_vuex/learnvuex/src/store/modules/moduleA.js
+++ 7_vuex/learnvuex/src/store/modules/moduleA.js
@@ -10,6 +10,32 @@ export default {
   state: {
     name: 'zhangsan'
   },
+  //state也可以定义成函数
+
+  //箭头函数的方式定义state，返回一个对象
+  // state: () => {
+  //   return {
+  //     cartList: ["电脑", "手机"]
+  //   }
+  // },
+
+  //对于箭头函数，如果函数体只有一行则可以省略大括号和return关键字
+  // state:() => {
+  // 	cartList: ["电脑2", "手机2"]
+  // },
+
+  //对比上面这种方式，用不用小括号()包裹都一样，效果好只是起到一个整体的作用，这里这个整体就是一个对象，因此可以省略
+  // state:() => ({
+  // 	cartList: ["电脑1", "手机1"]
+  // }),
+
+  //直接用一个对象定义对象
+  // state:{
+  // 		cartList: ["电脑", "手机"]
+  // }
+
+
+
   mutations: {
     //第一个参数的state是当前moduleA的是state，不是全局store里的state,第二个参数是用于存放参数的对象
     // this.$store.commit('updateName', 'lisi') 虽然, 我们的updateName是定义在module内部的.
