40472f91070774a6c4c1cfe1867eff556a3d1157
yinzhijie <yzjgogo@139.com>
2025-02-24 16:41:31 +0800

    提交一波代码

--- vuecli3study/src/views/my-tool/MyTool.vue
+++ vuecli3study/src/views/my-tool/MyTool.vue
@@ -16,6 +16,20 @@
     <div class="tool-button" @click="toVideoPlay()">
       旋转整个布局90度，模拟横屏
     </div>
+
+    <div class="tool-button">
+      VScode中配置ESlint+Prettier详细步骤:https://blog.csdn.net/hjl_and_djj/article/details/144674178 <br/>
+      (在无影新华移动端项目的基础上看git@192.168.3.55:shanbanyun/intelligent-book.git)注意：1,：当你发现打开某个文件夹(项目，可能是包含多个项目的文件)后,Shift+Alt+F格式化代码不生效时把.prettierrc文件放入目录即可；2：这个文章看'2 Eslint与Prettier设置'即可；3:intelligent-book项目并没有npm install prettier只是安装了prettier插件然后给vscode做响应的配置即可。<br/>
+    </div>
+
+    <div class="tool-button">
+      前端官网：https://developer.mozilla.org/zh-CN/docs/Web (进去后，左侧罗列了各种技术入口)<br/>
+      前端CSS官网：https://developer.mozilla.org/zh-CN/docs/Web/CSS<br/>
+      前端Html官网：https://developer.mozilla.org/zh-CN/docs/Web/HTML<br/>
+      前端JavaScript官网：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript<br/>
+      等等等（SVG,Web API，Web 媒体技术，Web 安全，Web 性能，WebDriver，XML，事件参考
+）<br/>
+    </div>
   </div>
 </template>
 
