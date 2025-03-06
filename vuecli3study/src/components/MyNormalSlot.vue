c763a0943e9c16867ab0ac6873c98d6d3d760fda
yzjgogo <yzjgogo@139.com>
2025-03-06 19:18:58 +0800

    插槽相关

---
+++ vuecli3study/src/components/MyNormalSlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot><span>我是默认的插槽内容</span></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MyNormalSlot',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
--- vuecli3study/src/views/my-slot/MySlot.vue
+++ vuecli3study/src/views/my-slot/MySlot.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
+    <h3>只有一个没名字的插槽</h3>
+    <MyNormalSlot></MyNormalSlot>
+    <MyNormalSlot>
+      <!-- 可以包裹多个元素 -->
+      <span>我替换默认的插槽内容</span>
+      <span>我替换默认的插槽内容</span>
+    </MyNormalSlot>
     <h3>具名插槽</h3>
+    <!-- 不适用插槽，全用默认的 -->
     <my-name-slot/>
-    <!--    使用slot="name" 的方式，可以不用<template/>包裹，也可以包裹-->
+    <!--    使用slot="name" 的方式-->
     <MyNameSlot>
-      <span slot="center" style="background-color: #6CD559">新内容1</span>
+      <span slot="center" style="background-color: #6CD559">使用slot="name" 的方式</span>
+    </MyNameSlot>
+    <!--    使用slot="name" 的方式并用<template/>包裹-->
+    <MyNameSlot>
+      <template slot="center">
+        <span style="background-color: #6CD559">使用slot="name" 的方式并用template包裹</span>
+      </template>
     </MyNameSlot>
 
     <!--      使用v-slot:name 的方式，需要用<template/>包裹，高版本的vue支持，2.6.xx就支持,当前项目是^2.6.14-->
     <MyNameSlot>
       <template v-slot:center>
-        <span slot="center" style="background-color: #2eaae0">新内容2</span>
+        <span slot="center" style="background-color: #2eaae0">使用v-slot:name 的方式，需要用template包裹，高版本的vue支持，2.6.xx就支持,当前项目是^2.6.14</span>
       </template>
     </MyNameSlot>
 
     <!--      使用#name 的方式，需要用<template/>包裹，这是v-slot:name的简写方式，高版本的vue支持，2.6.xx就支持,当前项目是^2.6.14-->
     <MyNameSlot>
       <template #center>
-        <span slot="center" style="background-color: #f8b38d">新内容3</span>
+        <span slot="center" style="background-color: #f8b38d">使用#name 的方式，需要用template包裹，这是v-slot:name的简写方式，高版本的vue支持，2.6.xx就支持,当前项目是^2.6.14</span>
       </template>
     </MyNameSlot>
 
@@ -28,6 +42,10 @@
 
 
     <h3>作用域插槽</h3>
+    
+    <!--作用域插槽:子组件提供数据，父组件提供样式-->
+<!--即：父组件替换插槽的标签，但是数据由子组件来提供-->
+
     <MyScopeSlot/>
 <!--    使用slot-scope='anystr' 的方式，anystr可任意定义，anyStrProperty要和MyScopeSlot里面定义的:anyStrProperty一样-->
     <MyScopeSlot>
@@ -52,11 +70,12 @@
 </template>
 
 <script>
+  import MyNormalSlot from "@/components/MyNormalSlot";
   import MyNameSlot from "@/components/MyNameSlot";
   import MyScopeSlot from "@/components/MyScopeSlot";
 export default {
   name: 'MySlot',
-  components: {MyNameSlot, MyScopeSlot},
+  components: {MyNormalSlot, MyNameSlot, MyScopeSlot},
   data() {
     return {
 
