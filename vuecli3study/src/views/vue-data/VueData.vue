05c06fc56cd5283d2853df44c0b0da1fe778c539
yzjgogo <yzjgogo@139.com>
2025-03-11 14:17:58 +0800

    提交一波代码

--- vuecli3study/src/views/vue-data/VueData.vue
+++ vuecli3study/src/views/vue-data/VueData.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <div>
+      <div class="line">$data 和 $options</div>
       <button @click="logData()">输出$data 和 $options</button>
 
-      <div class="line"></div>
+      <div class="line">$forceUpdate()</div>
 
       <div>
         $forceUpdate()：总之你发现页面实在是没响应式刷新，就用它，保证能刷新，比如你修改对象的属性或数值的元素等等；<br />
         https://cn.vuejs.org/api/component-instance.html#emit
       </div>
 
-      <div class="line"></div>
+      <div class="line">v-bind的用法</div>
 
       <div>
         向组件传入单个属性时，可以用v-bind:name或:name；向组件传入多个属性时，可以直接用v-bind传入一个对象，这个对象的每一个属性都是目标组件的属性或部分属性；例如这里是{
@@ -21,12 +22,14 @@
       <TestWin v-bind:age="18"></TestWin>
       <TestWin v-bind="testObj"></TestWin>
 
-      <div class="line"></div>
+      <div class="line">组件继承</div>
       <div>
         {{
           `子组件继承父组件：可用于修改父组件的行为满足自己的需求，一般当遇到第三方组件不足以满足自己的需求时，可以考虑这种方案。实际项目举例：无影(新华)移动版intelligent-book项目中，视频播放时全屏的方案是点击让整个布局旋转90度，但旋转后van-slider点击进度条某个区域后播放进度定位错乱了，因此可以考虑继承van-slider使其适配旋转的情况：(src\views\function\video-play\components\video-control.vue -> Slider -> src\components\wrong-question-notebook\CustomVideo\components\Slider.vue) -> 我已经拷贝到vuecli3study\src\views\vue-data\components\Slider.vue进去看详情`
         }}
       </div>
+
+      <div class="line">组件的attrs和listeners的使用</div>
 
       <div class="tool-button" @click="toAttrsAndListeners()">
         组件的attrs和listeners的使用；<br />
@@ -34,7 +37,16 @@
         listeners：孙子组件(任何层级的后代组件)想通过this.$emit()发送事件到爷爷或任何祖先组件时，可以在两端组件中间的每一层组件中使用v-on="$listeners"，这样就能做到事件层层向上传递到目标先辈组件。参考grandson发送事件，son里面用v-on="$listeners"修饰grandson，然后grandpa就能收到事件。
       </div>
 
+      <div class="line">vue2中路由转url</div>
       <div class="tool-button" @click="routeToUrl()">vue2中路由转url</div>
+
+      <div class="line">vuedraggable的用法</div>
+      <div>参考：drag01.pdf，drag02.pdf，drag03.pdf，drag04.pdf，drag05.pdf <br />
+        https://sortablejs.github.io/Vue.Draggable/#/simple <br />
+        https://github.com/SortableJS/Vue.Draggable <br />
+        https://www.itxst.com/sortablejs/neuinffi.html  <br />
+        https://www.itxst.com/vue-draggable-next/tutorial.html  <br />
+      </div>
     </div>
   </div>
 </template>
@@ -97,10 +109,11 @@ export default {
 
 <style lang="less" scoped>
 .line {
-  height: 1px;
-  width: 800px;
-  background-color: red;
-  margin: 20px;
+  width: 100%;
+  height: fit-content;
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
 }
 
 .tool-button {
